b4b5d9c69ec504367ce11e9b6dffb9ef94019e44
iwasz <iwasz@5bf182f4-304e-2cda-f505-2cb49601a70f>
2010-05-18 20:13:28 +0000

    Uporządkowany kod w main. Wyrzucony do specjalnych klas "BajkaApp" i "BajkaConfig".

---
+++ trunk/util/BajkaApp.h
@@ -0,0 +1,100 @@
+/****************************************************************************
+ *                                                                          *
+ *  Author : lukasz.iwaszkiewicz@tiliae.eu                                  *
+ *  ~~~~~~~~                                                                *
+ *  License : see COPYING file for details.                                 *
+ *  ~~~~~~~~~                                                               *
+ ****************************************************************************/
+
+#ifndef BAJKAAPP_H_
+#define BAJKAAPP_H_
+
+#include <String.h>
+#include <Reflection.h>
+
+namespace Controller {
+class IController;
+}
+
+namespace Util {
+
+/*
+ * Ta klasa jest tylko po to, żeby ułatwoć stosowanie bajki z kontenerem.
+ * Otóż kolejność pracy programu musi być następująca:
+ * - Utworzenie BajkaConfig.
+ * - Ustawienie jej pól.
+ * - Uruchomienie init ().
+ * Treaz OpenGL i SDL są zainicjowane, można dodawać obiekty. I dopero teraz
+ * można uruchomić pętlę programu:
+ * - BajkaApp run.
+ * INICJUJE OpenGL!
+ */
+class BajkaConfig {
+public:
+        __c (void)
+
+        BajkaConfig ();
+
+/*--------------------------------------------------------------------------*/
+
+        _m (init) void init ();
+
+/*------access methods------------------------------------------------------*/
+
+        bool getFullScreen () const { return fullScreen; }
+        _m (setFullScreen) void setFullScreen (bool fullScreen) { this->fullScreen = fullScreen; }
+
+        int getResX () const { return resX; }
+        _m (setResX) void setResX (int resX) { this->resX = resX; }
+
+        int getResY () const { return resY; }
+        _m (setResY)  void setResY (int resY) { this->resY = resY; }
+
+        const Core::String &getWindowCaption () const { return windowCaption; }
+        _m (setWindowCaption) void setWindowCaption (const Core::String &windowCaption) { this->windowCaption = windowCaption; }
+
+private:
+
+        bool fullScreen;
+        int resX;
+        int resY;
+        Core::String windowCaption;
+
+        _e (BajkaConfig)
+};
+
+/**
+ * Bajka application. It is bound to OpenGL and is not meant to
+ * be used with any other technologies (i.e. DirectX). Implements
+ * the main loop.
+ *
+ * CR : Kiedyś kiedyś : umożliwić stosowanie czegoś innego niż SDL.
+ */
+class BajkaApp {
+public:
+
+       __c (void)
+
+       void run (int argc, char **argv);
+       void loop ();
+       void destroy ();
+
+/*--------------------------------------------------------------------------*/
+
+       Ptr <BajkaConfig> getConfig () const { return config; }
+       _m (setConfig) void setConfig (Ptr <BajkaConfig> b) { config = b; }
+
+       Ptr <Controller::IController> getRootController () const { return rootController; }
+       _m (setRootController) void setRootController (Ptr <Controller::IController> r) { rootController = r; }
+
+private:
+
+       Ptr <BajkaConfig> config;
+       Ptr <Controller::IController> rootController;
+
+       _e (BajkaApp)
+};
+
+} // nam
+
+#	endif /* BAJKAAPP_H_ */
