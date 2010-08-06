e215fcfdea6e9e50c6b9178008e1237832d37662
iwasz <iwasz@5bf182f4-304e-2cda-f505-2cb49601a70f>
2010-08-06 16:52:44 +0000

    ParalaxModel

---
+++ trunk/model/ParalaxModel.cc
@@ -0,0 +1,18 @@
+/****************************************************************************
+ *                                                                          *
+ *  Author : lukasz.iwaszkiewicz@tiliae.eu                                  *
+ *  ~~~~~~~~                                                                *
+ *  License : see COPYING file for details.                                 *
+ *  ~~~~~~~~~                                                               *
+ ****************************************************************************/
+
+#include "ParalaxModel.h"
+
+namespace Model {
+
+void ParalaxModel::setMove (const Geometry::Point &p)
+{
+
+}
+
+}
