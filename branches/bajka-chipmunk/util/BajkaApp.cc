58bf8710de431a8f1dc5866c7b4bc4c8e919bad1
iwasz <iwasz@5bf182f4-304e-2cda-f505-2cb49601a70f>
2012-01-09 21:27:45 +0000

    Chipmunk

--- branches/bajka-chipmunk/util/BajkaApp.cc
+++ branches/bajka-chipmunk/util/BajkaApp.cc
@@ -18,28 +18,29 @@
 #include <cstdio>
 #include <boost/bind.hpp>
 
-#include "Image.h"
-#include "Model.h"
-#include "Events.h"
-#include "Util.h"
-#include "OpenGl.h"
-
-#include "sequence/Sequence.h"
-#include "mapping/Mapping.h"
-
-//#include "Sdl.h"
-#include "View.h"
+//#include "Image.h"
+//#include "Model.h"
+//#include "Events.h"
+//#include "Util.h"
+//#include "OpenGl.h"
+//
+//#include "sequence/Sequence.h"
+//#include "mapping/Mapping.h"
+//
+////#include "Sdl.h"
+//#include "View.h"
 
 #include "BajkaApp.h"
-#include "Controller.h"
-#include "Model.h"
+//#include "Controller.h"
+//#include "Model.h"
 
 namespace Util {
-namespace M = Model;
-namespace V = View;
-namespace C = Controller;
+//namespace M = Model;
+//namespace V = View;
+//namespace C = Controller;
 namespace R = Reflection;
-using namespace Event;
+//using namespace Event;
+
 
 /****************************************************************************/
 
@@ -51,6 +52,8 @@ BajkaConfig::BajkaConfig () : fullScreen (false), resX (640), resY (480), window
 
 void BajkaConfig::init ()
 {
+#if 1
+
         /* Initialize SDL for video output */
         if (SDL_Init (SDL_INIT_VIDEO) < 0) {
                 fprintf (stderr, "Unable to initialize SDL: %s\n", SDL_GetError ());
@@ -122,6 +125,90 @@ void BajkaConfig::init ()
         glPointSize (3);
         glGetFloatv (GL_POINT_SIZE, params);
         std::cerr << "GL_POINT_SIZE : " << params[0] << std::endl;
+
+/*--------------------------------------------------------------------------*/
+//
+//        glColor3f (0.0, 0.0, 1.0);
+//
+//        glBegin (GL_LINE_LOOP);
+//                glVertex2f (0, 0);
+//                glVertex2f (0, 100);
+//                glVertex2f (100, 100);
+//                glVertex2f (100, 0);
+//        glEnd ();
+//
+//        glClearColor((128.0f / 255.0f), 1.0f, 1.0f, 1.0f);
+
+#endif
+#if 0
+        if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+            return ;
+        }
+
+        SDL_GL_SetAttribute(SDL_GL_RED_SIZE,            8);
+        SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,          8);
+        SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,           8);
+        SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE,          8);
+
+        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,          16);
+        SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE,            32);
+
+        SDL_GL_SetAttribute(SDL_GL_ACCUM_RED_SIZE,        8);
+        SDL_GL_SetAttribute(SDL_GL_ACCUM_GREEN_SIZE,    8);
+        SDL_GL_SetAttribute(SDL_GL_ACCUM_BLUE_SIZE,        8);
+        SDL_GL_SetAttribute(SDL_GL_ACCUM_ALPHA_SIZE,    8);
+
+        SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS,  1);
+
+        SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES,  2);
+
+        if((/*Surf_Display = */SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_GL_DOUBLEBUFFER | SDL_OPENGL)) == NULL) {
+            return;
+        }
+
+        glClearColor(0, 0, 0, 0);
+
+        glViewport(0, 0, 640, 480);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+
+        glOrtho(0, 640, 480, 0, 1, -1);
+
+        glMatrixMode(GL_MODELVIEW);
+
+        glEnable(GL_TEXTURE_2D);
+
+        glLoadIdentity();
+
+/*--------------------------------------------------------------------------*/
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glLoadIdentity();
+
+        glBegin(GL_QUADS);
+            glColor3f(1, 0, 0); glVertex3f(0, 0, 0);
+            glColor3f(1, 1, 0); glVertex3f(100, 0, 0);
+            glColor3f(1, 0, 1); glVertex3f(100, 100, 0);
+            glColor3f(1, 1, 1); glVertex3f(0, 100, 0);
+        glEnd();
+
+
+//        glColor3f (0.0, 0.0, 1.0);
+//
+//        glBegin (GL_LINE_LOOP);
+//                glVertex2f (0, 0);
+//                glVertex2f (0, 100);
+//                glVertex2f (100, 100);
+//                glVertex2f (100, 0);
+//        glEnd ();
+
+        glClearColor((128.0f / 255.0f), 1.0f, 1.0f, 1.0f);
+
+        SDL_GL_SwapBuffers();
+
+        return;
+#endif
 }
 
 /*##########################################################################*/
@@ -132,17 +219,27 @@ void BajkaApp::loop ()
 
         while (!done) {
 
-                // Generuj eventy.
-                for (Event::DispatcherList::const_iterator i = dispatchers->begin (); i != dispatchers->end (); i++) {
-                        (*i)->run ();
+                glMatrixMode (GL_MODELVIEW);
+                glLoadIdentity ();
+                glClearColor (0.35, 0.75, 1.0, 1.0);
+                glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+//                // Generuj eventy.
+//                for (Event::DispatcherList::const_iterator i = dispatchers->begin (); i != dispatchers->end (); i++) {
+//                        (*i)->run ();
+//                }
+//
+                if (model) {
+                        model->update ();
                 }
 
-                if (rootController) {
-                        rootController->draw ();
-                }
+                cpSpaceStep (Model::CPSpace::getSpace (), 1.0 / 60.0);
 
                 afterEvents ();
 
+                glFlush ();
+                // swap buffers to display, since we're double buffered.
+                SDL_GL_SwapBuffers ();
                 // Tak śmiga, że damy delay
                 SDL_Delay (5);
         }
@@ -171,28 +268,300 @@ void BajkaApp::destroy ()
 
 /****************************************************************************/
 
-void BajkaApp::setRootController (Ptr <Controller::IController> r)
+void BajkaApp::setModel (Ptr <Model::IModel> m)
 {
-        if (rootController) {
-                disconnect (rootController);
+        model = m;
+//        if (rootController) {
+//                disconnect (rootController);
+//        }
+//
+//        rootController = r;
+//        connect (r);
+}
+//
+///****************************************************************************/
+//
+//void BajkaApp::connect (Ptr <IObserver> o)
+//{
+//        std::for_each (dispatchers->begin (), dispatchers->end (), boost::bind (&IDispatcher::addObserver, _1, o));
+//}
+//
+///****************************************************************************/
+//
+//void BajkaApp::disconnect (Ptr <IObserver> o)
+//{
+//        std::for_each (dispatchers->begin (), dispatchers->end (), boost::bind (&IDispatcher::removeObserver, _1, o));
+//}
+
+/*##########################################################################*/
+} // Nam
+
+
+namespace View {
+
+void Widget::preUpdate (Model::IModel *model)
+{
+        if (!visible) {
+                return;
         }
 
-        rootController = r;
-        connect (r);
+        glPushMatrix ();
+
+        if (model) {
+                glMultMatrixd (model->getMatrix ());
+        }
 }
 
-/****************************************************************************/
+//void Widget::update (Model::IModel *model)
+//{
+//
+//}
 
-void BajkaApp::connect (Ptr <IObserver> o)
+void Widget::postUpdate (Model::IModel *model)
 {
-        std::for_each (dispatchers->begin (), dispatchers->end (), boost::bind (&IDispatcher::addObserver, _1, o));
+        if (!visible) {
+                return;
+        }
+
+        glPopMatrix ();
 }
 
-/****************************************************************************/
-
-void BajkaApp::disconnect (Ptr <IObserver> o)
+void Rectangle::update (Model::IModel *model)
 {
-        std::for_each (dispatchers->begin (), dispatchers->end (), boost::bind (&IDispatcher::removeObserver, _1, o));
+        // TODO Do wywalenia.
+        glColor3f (1.0, 0.0, 0.0);
+
+        // TODO Lub dynamic, dio zastanowienia się.
+        // TODO Kastować na ogólniejszy typ.
+        Model::Box *box = static_cast <Model::Box *> (model);
+
+        double x = box->getPosition ().getX ();
+        double y = box->getPosition ().getY ();
+
+        glBegin (GL_LINE_LOOP);
+                glVertex2f (x, y);
+                glVertex2f (x, y + box->getHeight ());
+                glVertex2f (x + box->getHeight (), y + box->getHeight ());
+                glVertex2f (x + box->getHeight (), y);
+        glEnd ();
+}
+
+void Circle::update (Model::IModel *model)
+{
+        // TODO Do wywalenia.
+        glColor3f (0.0, 1.0, 0.0);
+
+        // TODO Lub dynamic, dio zastanowienia się.
+        // TODO Kastować na ogólniejszy typ.
+//        Model::CPCircle *box = static_cast <Model::CPCircle *> (model);
+
+        double x = model->getPosition ().getX ();
+        double y = model->getPosition ().getY ();
+
+        glBegin (GL_LINE_LOOP);
+                glVertex2f (x, y);
+                glVertex2f (x, y + 30);
+                glVertex2f (x + 30, y + 30);
+                glVertex2f (x + 30, y);
+        glEnd ();
 }
 
 } // Nam
+
+namespace Model {
+
+bool AbstractModel::update ()
+{
+        if (controller) {
+                controller->preUpdate (this, view);
+                controller->update (this, view);
+        }
+
+        if (view) {
+                view->preUpdate (this);
+                view->update (this);
+        }
+
+        std::for_each (begin (), end (), boost::mem_fn (&IModel::update));
+
+        if (controller) {
+                controller->postUpdate (this, view);
+        }
+
+        if (view) {
+                view->postUpdate (this);
+        }
+}
+
+
+cpSpace *CPSpace::space = NULL;
+
+CPSpace::CPSpace ()
+{
+        assert (!space);
+        space = cpSpaceNew ();
+}
+
+CPSpace::~CPSpace ()
+{
+        cpSpaceFree (space);
+}
+
+Geometry::Vector CPSpace::getGravity () const
+{
+        cpVect g = cpSpaceGetGravity (space);
+        return Geometry::Vector (g.x, g.y);
+}
+
+void CPSpace::setGravity (Geometry::Vector const &g)
+{
+        cpSpaceSetGravity (space, cpv (g.getX (), g.getY ()));
+}
+
+double const *CPSpace::getMatrix () const
+{
+        static Geometry::AffineMatrix one;
+        return one.data ().begin ();
+}
+
+/*--------------------------------------------------------------------------*/
+
+CPBody::CPBody ()
+{
+        body = cpSpaceAddBody (CPSpace::getSpace (), cpBodyNew (1, 1));
+}
+
+CPBody::~CPBody ()
+{
+        cpBodyFree (body);
+}
+
+Geometry::Point CPBody::getPosition () const
+{
+        cpVect v = cpBodyGetPos (body);
+        return Geometry::Point (v.x, v.y);
+}
+
+void CPBody::setPosition (Geometry::Point const &p)
+{
+        cpBodySetPos (body, cpv (p.getX (), p.getY ()));
+        cpSpaceReindexShapesForBody (CPSpace::getSpace (), body);
+}
+
+double CPBody::getAngle () const
+{
+        return cpBodyGetAngle (body);
+}
+
+void CPBody::setAngle (double a)
+{
+        cpBodySetAngle (body, a);
+        cpSpaceReindexShapesForBody (CPSpace::getSpace (), body);
+}
+
+double const *CPBody::getMatrix () const
+{
+        // TODO!
+        static Geometry::AffineMatrix one;
+        return one.data ().begin ();
+}
+
+double CPBody::getMass () const
+{
+        return cpBodyGetMass (body);
+}
+
+void CPBody::setMass (double m)
+{
+        cpBodySetMass (body, m);
+}
+
+double CPBody::getInertia () const
+{
+        return cpBodyGetMoment (body);
+}
+
+void CPBody::setInertia (double i)
+{
+        cpBodySetMoment (body, i);
+}
+
+void CPBody::addInertia (double i)
+{
+        double tmp = cpBodyGetMoment (body);
+        cpBodySetMoment (body, i + tmp);
+}
+
+/****************************************************************************/
+
+CPShape::~CPShape ()
+{
+        cpShapeFree (shape);
+}
+
+void CPShape::parentCallback (IModel *m)
+{
+        CPModel::parentCallback (m);
+        // TODO można zrobic cast w zalezności od define DEBUG/RELEASE.
+        CPBody *body = static_cast <CPBody *> (m);
+        double inertia = calculateInertia (body->getMass ());
+        body->addInertia (inertia);
+}
+
+double const *CPShape::getMatrix () const
+{
+        // TODO!
+        static Geometry::AffineMatrix one;
+        return one.data ().begin ();
+}
+
+/****************************************************************************/
+
+CPCircle::CPCircle ()
+{
+
+}
+
+double CPCircle::calculateInertia (double mass) const
+{
+        return cpMomentForCircle (mass, 0, getRadius (), cpv (getPosition ().getX (), getPosition ().getY ()));
+}
+
+void CPCircle::parentCallback (IModel *m)
+{
+        CPBody *b = static_cast <CPBody *> (m);
+        shape = cpCircleShapeNew (b->getBody (), radius, cpv (position.getX (), position.getY ()));
+        CPShape::parentCallback (m);
+}
+
+double CPCircle::getRadius () const
+{
+        return cpCircleShapeGetRadius (shape);
+}
+
+void CPCircle::setRadius (double r)
+{
+        if (getParent ()) {
+                throw Util::OperationNotSupportedException ();
+        }
+
+        radius = r;
+}
+
+void CPCircle::setPosition (Geometry::Point const &p)
+{
+        if (getParent ()) {
+                throw Util::OperationNotSupportedException ();
+        }
+
+        position = p;
+}
+
+Geometry::Point CPCircle::getPosition () const
+{
+        cpVect v = cpCircleShapeGetOffset (shape);
+        return Geometry::Point (v.x, v.y);
+}
+
+
+}
--- branches/bajka-chipmunk/util/BajkaApp.h
+++ branches/bajka-chipmunk/util/BajkaApp.h
@@ -9,13 +9,393 @@
 #ifndef BAJKAAPP_H_
 #define BAJKAAPP_H_
 
-#include <String.h>
+#include "../model/geometry/Utils.h"
 #include <Reflection.h>
+#include "../model/geometry/Point.h"
+#include <chipmunk.h>
 
-#include "IDispatcher.h"
+//using Geometry::AffineMatrix;
+//#include "IDispatcher.h"
+
+//namespace Controller {
+//class IController;
+//}
+
+namespace Util {
+
+struct OperationNotSupportedException : public std::exception {
+
+        virtual ~OperationNotSupportedException () throw () {}
+        virtual const char* what() const throw () { return "Bajka not nupported operation"; }
+
+};
+
+}
+
+namespace Model {
+class IModel;
+}
+
+namespace View {
+
+/**
+ * Widok.
+ */
+struct IView : public Core::Object {
+
+        virtual ~IView () {}
+
+        virtual void preUpdate (Model::IModel *) = 0;
+        virtual void update (Model::IModel *) = 0;
+        virtual void postUpdate (Model::IModel *) = 0;
+
+};
+
+class Widget : public IView {
+public:
+
+        Widget () : visible (true) {}
+        virtual ~Widget () {}
+
+        virtual void preUpdate (Model::IModel *);
+        virtual void postUpdate (Model::IModel *);
+
+        bool getVisible () const { return visible; }
+        /*_m (setVisible) */void setVisible (bool v) { visible = v; }
+
+private:
+
+        bool visible;
+
+};
+
+
+/**
+ * Rysuje prostokąt.
+ * \ingroup View
+ */
+class Rectangle : public Widget {
+public:
+//        __c (void)
+//        _b ("AbstractWidget")
+
+        virtual ~Rectangle () {}
+
+        virtual void update (Model::IModel *model);
+
+//// Żeby nie trzeba było kastowac modelu na box w pochodnych.
+//protected:
+//
+//        Model::Box *box;
+//        _e (Rectangle)
+};
+
+/**
+ * Rysuje prostokąt.
+ * \ingroup View
+ */
+class Circle : public Widget {
+public:
+
+        virtual ~Circle () {}
+        virtual void update (Model::IModel *model);
+};
+
+}
+
 
 namespace Controller {
-class IController;
+
+/**
+ *
+ */
+struct IController : public Core::Object {
+
+        virtual ~IController () {}
+
+        virtual bool preUpdate (Model::IModel *m, View::IView *v) = 0;
+        virtual bool update (Model::IModel *m, View::IView *v) = 0;
+        virtual bool postUpdate (Model::IModel *m, View::IView *v) = 0;
+
+};
+
+struct EmptyController : public IController {
+
+        virtual ~EmptyController () {}
+
+        virtual bool preUpdate (Model::IModel *m, View::IView *v) {}
+        virtual bool update (Model::IModel *m, View::IView *v) {}
+        virtual bool postUpdate (Model::IModel *m, View::IView *v) {}
+
+};
+
+}
+
+namespace Model {
+
+typedef std::vector <IModel *> ModelVector;
+
+struct IModel : public Core::Object {
+
+        virtual ~IModel () {}
+
+        /**
+         * Pozycja tego modelu we współrzędnych rodzica. Jest to pozycja
+         * pewnego punktu w tym modelu (na przykład środek okręgu).
+         */
+        virtual Geometry::Point getPosition () const = 0;
+        virtual void setPosition (Geometry::Point const &position) = 0;
+
+        virtual double getAngle () const = 0;
+        virtual void setAngle (double a) = 0;
+
+        virtual double const *getMatrix () const = 0;
+
+/*--------------------------------------------------------------------------*/
+
+        virtual bool update () = 0;
+
+/*--------------------------------------------------------------------------*/
+
+        virtual IModel *getParent () = 0;
+
+        /**
+         * Także callback;
+         * @param m
+         */
+        virtual void setParent (IModel *m) = 0;
+        virtual void parentCallback (IModel *m) = 0;
+
+        virtual ModelVector &getChildren () = 0;
+        virtual void setChildren (ModelVector const &) = 0;
+        virtual void addChild (IModel *m) = 0;
+        virtual ModelVector::iterator begin () = 0;
+        virtual ModelVector::iterator end () = 0;
+
+/*--------------------------------------------------------------------------*/
+
+        virtual View::IView *getView () = 0;
+        virtual void setView (View::IView *v) = 0;
+
+        virtual Controller::IController *getController () = 0;
+        virtual void setController (Controller::IController *c) = 0;
+
+};
+
+class AbstractModel : public IModel {
+public:
+
+        AbstractModel () : parent (0), view (0), controller (0) {}
+        virtual ~AbstractModel () {}
+
+        virtual bool update ();
+
+/*--------------------------------------------------------------------------*/
+
+        IModel *getParent () { return parent; }
+        void setParent (IModel *m) { parent = m; }
+        void parentCallback (IModel *m) {}
+
+        ModelVector &getChildren () { return children; }
+        // Todo pętla setParent
+        void setChildren (ModelVector const &c) { children = c; }
+        void addChild (IModel *m) { children.push_back (m); m->setParent (this); m->parentCallback (this); }
+        ModelVector::iterator begin () { return children.begin (); }
+        ModelVector::iterator end () { return children.end (); }
+
+/*--------------------------------------------------------------------------*/
+
+        View::IView *getView () { return view; }
+        void setView (View::IView *v) { view = v; }
+
+        Controller::IController *getController () { return controller; }
+        void setController (Controller::IController *c) { controller = c; }
+
+private:
+
+        IModel *parent;
+        ModelVector children;
+        View::IView *view;
+        Controller::IController *controller;
+
+};
+
+
+class CPModel : public AbstractModel {
+
+};
+
+class CPSpace : public CPModel {
+public:
+
+        CPSpace ();
+        virtual ~CPSpace ();
+
+        static cpSpace *getSpace () { return space; }
+
+/*--------------------------------------------------------------------------*/
+
+        Geometry::Point getPosition () const { return Geometry::Point (); }
+        void setPosition (Geometry::Point const &position) { throw Util::OperationNotSupportedException ();  }
+
+        double getAngle () const { return 0; }
+        void setAngle (double a) { throw Util::OperationNotSupportedException ();  }
+
+        double const *getMatrix () const;
+
+/*--------------------------------------------------------------------------*/
+
+        Geometry::Vector getGravity () const;
+        void setGravity (Geometry::Vector const &g);
+
+private:
+
+        static cpSpace *space;
+
+};
+
+class CPBody : public CPModel {
+public:
+
+        CPBody ();
+        virtual ~CPBody ();
+
+        Geometry::Point getPosition () const;
+        void setPosition (Geometry::Point const &position);
+
+        double getAngle () const;
+        void setAngle (double a);
+
+        double const *getMatrix () const;
+
+/*--------------------------------------------------------------------------*/
+
+        double getMass () const;
+        void setMass (double m);
+
+        double getInertia () const;
+        void setInertia (double i);
+        void addInertia (double i);
+
+        cpBody *getBody () { return body; }
+        cpBody const *getBody () const { return body; }
+
+private:
+
+        cpBody *body;
+
+
+};
+
+class CPRigidBody : public CPBody {
+
+};
+
+class CPStaticBody : public CPRigidBody {
+
+};
+
+class CPShape : public CPModel {
+public:
+
+        CPShape () : shape (0) {}
+        virtual ~CPShape ();
+
+        virtual double calculateInertia (double mass) const = 0;
+        virtual void parentCallback (IModel *m);
+
+        double const *getMatrix () const;
+
+protected:
+
+        cpShape *shape;
+
+};
+
+class CPCircle : public CPShape {
+public:
+
+        CPCircle ();
+        virtual ~CPCircle () {}
+
+        double getRadius () const;
+        void setRadius (double r);
+
+        Geometry::Point getPosition () const;
+        void setPosition (Geometry::Point const &p);
+
+        double getAngle () const { return 0; }
+        void setAngle (double) {}
+
+        /*--------------------------------------------------------------------------*/
+
+        double calculateInertia (double mass) const;
+        virtual void parentCallback (IModel *m);
+
+private:
+
+        double radius;
+        Geometry::Point position;
+
+};
+
+class CPSegment : public CPShape {
+
+};
+
+class CPPolygon : public CPShape {
+
+};
+
+class CPBox : public CPPolygon {
+public:
+
+//        CPBox () : width (0), height (0) {}
+//
+//        double getWidth () const { return width; }
+//        void setWidth (double w) { width = w; }
+//
+//        double getHeight () const { return height; }
+//        void setHeight (double h) { height = h; }
+//
+//private:
+//
+//        double width;
+//        double height;
+
+};
+
+class Box : public AbstractModel {
+public:
+
+        Box (double x, double y, double w, double h) : position (x, y), width (w), height (h) {}
+        Box () : width (0), height (0) {}
+        virtual ~Box () {}
+
+        Geometry::Point getPosition () const { return position; }
+        void setPosition (Geometry::Point const &p) { position = p; }
+
+        // TODO
+        double getAngle () const { throw 1; }
+        void setAngle (double a) { matrix.rotate (a); }
+
+        double const *getMatrix () const { return matrix.data ().begin (); }
+
+        double getWidth () const { return width; }
+        void setWidth (double w) { width = w; }
+
+        double getHeight () const { return height; }
+        void setHeight (double h) { height = h; }
+
+private:
+
+        Geometry::Point position;
+        double width;
+        double height;
+        Geometry::AffineMatrix matrix;
+
+};
+
 }
 
 namespace Util {
@@ -52,15 +432,15 @@ public:
         int getResY () const { return resY; }
         _m (setResY)  void setResY (int resY) { this->resY = resY; }
 
-        const Core::String &getWindowCaption () const { return windowCaption; }
-        _m (setWindowCaption) void setWindowCaption (const Core::String &windowCaption) { this->windowCaption = windowCaption; }
+        const std::string &getWindowCaption () const { return windowCaption; }
+        _m (setWindowCaption) void setWindowCaption (const std::string &windowCaption) { this->windowCaption = windowCaption; }
 
 private:
 
         bool fullScreen;
         int resX;
         int resY;
-        Core::String windowCaption;
+        std::string windowCaption;
 
         _e (BajkaConfig)
 };
@@ -78,7 +458,7 @@ public:
 
        __c (void)
 
-       BajkaApp () : dispatchers (new Event::DispatcherList) {}
+       BajkaApp () /*: dispatchers (new Event::DispatcherList)*/ {}
        virtual ~BajkaApp () {}
 
        _m (loop) void loop ();
@@ -96,31 +476,31 @@ public:
        Ptr <BajkaConfig> getConfig () const { return config; }
        _m (setConfig) void setConfig (Ptr <BajkaConfig> b) { config = b; }
 
-       /// Głowny kontroler.
-       Ptr <Controller::IController> getRootController () const { return rootController; }
+       /// Głowny model.
+       Ptr <Model::IModel> getModel () const { return model; }
 
        /**
         * 1. Odłącza poprzedni rootControloer od dispatcherów.
         * 2. Ustawia główny kontroler.
         * 3. Podłącza ten nowy do wszystkich dispatcherów.
         */
-       _m (setRootController) void setRootController (Ptr <Controller::IController> r);
-
-       Ptr <Event::DispatcherList> getDispatchers () const { return dispatchers; }
-       _m (setDispatchers) void setDispatchers (Ptr <Event::DispatcherList> d) { dispatchers = d; }
+       _m (setModel) void setModel (Ptr <Model::IModel> model);
+//
+//       Ptr <Event::DispatcherList> getDispatchers () const { return dispatchers; }
+//       _m (setDispatchers) void setDispatchers (Ptr <Event::DispatcherList> d) { dispatchers = d; }
 
 /*--------------------------------------------------------------------------*/
 
-       /// Podłącza o do wszystkich dispatcherów.
-       void connect (Ptr <Event::IObserver> o);
-       /// Odłącza o od wszystkich dispatcherów.
-       void disconnect (Ptr <Event::IObserver> o);
+//       /// Podłącza o do wszystkich dispatcherów.
+//       void connect (Ptr <Event::IObserver> o);
+//       /// Odłącza o od wszystkich dispatcherów.
+//       void disconnect (Ptr <Event::IObserver> o);
 
 private:
 
        Ptr <BajkaConfig> config;
-       Ptr <Controller::IController> rootController;
-       Ptr <Event::DispatcherList> dispatchers;
+       Ptr <Model::IModel> model;
+//       Ptr <Event::DispatcherList> dispatchers;
 
        _e (BajkaApp)
 };
