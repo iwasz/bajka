c3e94b4b91ffdbc5161bddc96eddf70855d64dae
Łukasz Iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2012-01-26 13:55:36 +0000

    Więcej prymitywów

---
+++ branches/bajka-chipmunk/model/static/Static.h
@@ -0,0 +1,13 @@
+/****************************************************************************
+ *                                                                          *
+ *  Author : lukasz.iwaszkiewicz@gmail.com                                  *
+ *  ~~~~~~~~                                                                *
+ *  License : see COPYING file for details.                                 *
+ *  ~~~~~~~~~                                                               *
+ ****************************************************************************/
+
+#ifndef BASIC_H_
+#define BASIC_H_
+
+
+#	endif /* BASIC_H_ */
