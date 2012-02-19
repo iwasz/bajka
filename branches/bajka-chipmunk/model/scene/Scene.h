ab04ee2eb2d71d70a6353835a18a8dfea178cded
iwasz <iwasz@5bf182f4-304e-2cda-f505-2cb49601a70f>
2012-02-19 22:04:25 +0000

    ModelManager

---
+++ branches/bajka-chipmunk/model/scene/Scene.h
@@ -0,0 +1,17 @@
+/****************************************************************************
+ *                                                                          *
+ *  Author : lukasz.iwaszkiewicz@gmail.com                                  *
+ *  ~~~~~~~~                                                                *
+ *  License : see COPYING file for details.                                 *
+ *  ~~~~~~~~~                                                               *
+ ****************************************************************************/
+
+#ifndef BAJKA_SCENE_H_
+#define BAJKA_SCENE_H_
+
+class Scene {
+public:
+        virtual ~Scene () {}
+};
+
+#	endif /* SCENE_H_ */
