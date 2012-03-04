6790dcd840bddf03621de660df439e36c776d8a5
Łukasz Iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2012-03-04 13:07:30 +0000

    Mouse pointer

---
+++ branches/bajka-chipmunk/demo/PointerController.h
@@ -0,0 +1,36 @@
+/****************************************************************************
+ *                                                                          *
+ *  Author : lukasz.iwaszkiewicz@gmail.com                                  *
+ *  ~~~~~~~~                                                                *
+ *  License : see COPYING file for details.                                 *
+ *  ~~~~~~~~~                                                               *
+ ****************************************************************************/
+
+#ifndef POINTERCONTROLLER_H_
+#define POINTERCONTROLLER_H_
+
+#include "../controller/EmptyController.h"
+
+namespace Demo {
+
+class PointerController : public Controller::EmptyController {
+public:
+        C__ (void)
+        b_ ("EmptyController")
+
+        virtual ~PointerController () {}
+        virtual bool onMouseMotion (Event::MouseMotionEvent *e, Model::IModel *m, View::IView *v);
+
+        S_ (setPointer)
+        void setPointer (Model::IModel *p) { pointer = p; }
+
+private:
+
+        Model::IModel *pointer;
+
+        E_ (PointerController)
+};
+
+} /* namespace Demo */
+
+#	endif /* POINTERCONTROLLER_H_ */
