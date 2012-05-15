59ab8498272f463c24afcb6ac5117cc10497350a
iwasz <iwasz@5bf182f4-304e-2cda-f505-2cb49601a70f>
2012-05-15 19:52:18 +0000

    OK

---
+++ hourglass/src/Dummy.h
@@ -0,0 +1,20 @@
+/****************************************************************************
+ *                                                                          *
+ *  Author : lukasz.iwaszkiewicz@gmail.com                                  *
+ *  ~~~~~~~~                                                                *
+ *  License : see COPYING file for details.                                 *
+ *  ~~~~~~~~~                                                               *
+ ****************************************************************************/
+
+#ifndef DUMMY_H_
+#define DUMMY_H_
+
+namespace Event {
+
+class Dummy {
+public:
+        virtual ~Dummy () {}
+};
+
+} /* namespace Event */
+#endif /* DUMMY_H_ */
