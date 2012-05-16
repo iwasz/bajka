c6c620a4b005394f6a31c63f3b93b3d8c2a5c465
iwasz <iwasz@5bf182f4-304e-2cda-f505-2cb49601a70f>
2012-05-16 22:09:37 +0000

    SVG path parser - rudimentary

--- library/trunk/src/geometry/LineString.cc
+++ library/trunk/src/geometry/LineString.cc
@@ -7,18 +7,68 @@
  ****************************************************************************/
 
 #include "LineString.h"
+#include <boost/tokenizer.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/arithmetic/arithmetic.hpp>
+#include "../util/Exceptions.h"
 
 namespace Geometry {
+using namespace boost::geometry;
+using namespace boost;
+using namespace boost::algorithm;
 
 Ptr <LineString> stringToLineString (std::string const &s)
 {
         Ptr <LineString> ret = boost::make_shared <LineString> ();
-        ret->push_back (makePoint (0, 0));
-        ret->push_back (makePoint (40, 40));
-        ret->push_back (makePoint (40, -40));
-        ret->push_back (makePoint (-40, -40));
-        ret->push_back (makePoint (-40, 40));
+
+        typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+        tokenizer tok (s, boost::char_separator<char> (" "));
+
+        Point pen = {0, 0};
+        std::string command;
+        int pointNumber = 0;
+
+        for (tokenizer::iterator i = tok.begin (); i != tok.end (); ++i) {
+                std::string token = *i;
+                bool firstCommand = (i == tok.begin ());
+
+                if (is_any_of ("mMlLzZcC") (token[0])) {
+                        command = token;
+                }
+                else {
+                        Point p = stringToPoint (token);
+
+                        if (command == "m") {
+                                add_point (pen, p);
+                                command = "l";
+                                ret->push_back (pen);
+                        }
+                        else if (command == "M") {
+                                pen = p;
+                                // TODO multi-path nie będzie teraz obsługiwny.
+                                command = (firstCommand) ? ("l") : ("L");
+                                ret->push_back (pen);
+                        }
+                        else if (command == "l") {
+                                add_point (pen, p);
+                                ret->push_back (pen);
+                        }
+                        else if (command == "L") {
+                                ret->push_back (p);
+                        }
+                        // Bezier rysowany jako zwykły odcinek.
+                        else if (command == "c" || command == "C") {
+                                if (pointNumber++ >= 2) {
+                                        ret->push_back ((command == "c") ? pen : p);
+                                        pointNumber = 0;
+                                }
+                        }
+                }
+        }
+
         return ret;
+
 }
 
 /**
