5211dc0add532b781352c81744f1cb1bd2365ed4
iwasz <iwasz@5bf182f4-304e-2cda-f505-2cb49601a70f>
2012-05-18 13:11:02 +0000

    CPLineString

--- library/trunk/src/model/physics/CPLineString.cc
+++ library/trunk/src/model/physics/CPLineString.cc
@@ -6,33 +6,84 @@
  *  ~~~~~~~~~                                                               *
  ****************************************************************************/
 
-#include "LineString.h"
+#ifdef USE_CHIPMUNK
+#include <chipmunk.h>
+#include "CPLineString.h"
+#include "Segment.h"
+#include "Body.h"
+#include "Space.h"
 
 namespace Model {
 
-Geometry::Box LineString::getBoundingBoxImpl (Geometry::AffineMatrix const &transformation) const
+struct CPLineStringImpl {
+
+        CPLineStringImpl () : hasPoint (false) {}
+
+        std::vector <cpShape *> shapes;
+        Geometry::Point startPoint;
+        bool hasPoint;
+};
+
+CPLineString::CPLineString ()
+{
+        impl = new CPLineStringImpl;
+}
+
+CPLineString::~CPLineString()
+{
+        delete impl;
+}
+
+void CPLineString::addPoint (Geometry::Point const &p)
+{
+        if (!impl->hasPoint) {
+                impl->startPoint = p;
+                impl->hasPoint = true;
+                return;
+        }
+
+        // TODO Kast zalezny od macra RELEASE / DEBUG
+        Body *b = dynamic_cast <Body *> (getParent ());
+        cpShape *shape = cpSegmentShapeNew (b->getBody (), cpv (impl->startPoint.x, impl->startPoint.y) , cpv (p.x, p.y), radius);
+
+        impl->shapes.push_back (shape);
+
+        shape = cpSpaceAddShape (Space::getSpace(), shape);
+        cpShapeSetUserData (shape, this);
+
+        // TODO czy to powinna być masa całego body?
+        b->addInertia (cpMomentForSegment (b->getMass (), cpv (impl->startPoint.x, impl->startPoint.y) , cpv (p.x, p.y)));
+
+        impl->startPoint = p;
+}
+
+void CPLineString::setData (Ptr <Geometry::LineString> d)
+{
+        std::for_each (d->begin (), d->end (), boost::bind (&CPLineString::addPoint, this, _1));
+}
+
+Geometry::Box CPLineString::getBoundingBoxImpl (Geometry::AffineMatrix const &transformation) const
 {
 
 }
 
-bool LineString::contains (Geometry::Point const &p) const
+bool CPLineString::contains (Geometry::Point const &p) const
 {
 
 }
 
-Geometry::Point LineString::computeCenter () const
+Geometry::Point CPLineString::computeCenter () const
 {
 
 }
 
-void *LineString::getPointArray () const
+void *CPLineString::getPointArray () const
 {
-        return &data->operator [] (0);
 }
 
-size_t LineString::getNumberOfPoints () const
+size_t CPLineString::getNumberOfPoints () const
 {
-        return data->size ();
 }
 
 } /* namespace Model */
+#endif
